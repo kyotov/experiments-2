59c5a81cf17b610c875db9f7d244e53e23b57217
Ashish Thakkar <ashishvthakkar@gmail.com>
2021-12-19 16:32:59 -0500

    Make guard format consistent

--- type_size_optimizations/complex_call.h
+++ type_size_optimizations/complex_call.h
@@ -1,12 +1,12 @@
 #ifndef EXPERIMENTS_TYPE_SIZE_OPTIMIZATIONS_COMPLEX_CALL_H
+#define EXPERIMENTS_TYPE_SIZE_OPTIMIZATIONS_COMPLEX_CALL_H
 
 #include "price_setter.h"
 
 namespace code_experiments {
 
 void ComplexCall();
-  
-} // namespace code_experiments
 
-#define EXPERIMENTS_TYPE_SIZE_OPTIMIZATIONS_COMPLEX_CALL_H
+}  // namespace code_experiments
+
 #endif
--- type_size_optimizations/simple_call.h
+++ type_size_optimizations/simple_call.h
@@ -1,12 +1,12 @@
 #ifndef EXPERIMENTS_TYPE_SIZE_OPTIMIZATIONS_SIMPLE_CALL_H
+#define EXPERIMENTS_TYPE_SIZE_OPTIMIZATIONS_SIMPLE_CALL_H
 
 #include "price_setter.h"
 
 namespace code_experiments {
 
 void SimpleCall();
-  
-} // namespace code_experiments
 
-#define EXPERIMENTS_TYPE_SIZE_OPTIMIZATIONS_SIMPLE_CALL_H
+}  // namespace code_experiments
+
 #endif
